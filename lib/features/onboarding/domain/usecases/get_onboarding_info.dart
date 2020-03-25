d0151cda63220e40e6c02c6d7dbeda3ee15e9370
Winston <winstonyeo99@yahoo.com>
2020-03-25 08:31:43 +0800

    🚀 Add next() method to pass test

--- lib/features/onboarding/domain/usecases/get_onboarding_info.dart
+++ lib/features/onboarding/domain/usecases/get_onboarding_info.dart
@@ -7,9 +7,22 @@ import 'package:flutter/material.dart';
 class getOnboardingInfo {
   getOnboardingInfo({this.repository});
   OnboardingRepository repository;
+  ScreenNumber screenNum = ScreenNumber.zero;
 
   Future<Either<Failure, OnboardInfo>> call(
       {@required ScreenNumber screenNumber}) async {
     return await repository.getOnboardingInfo(screenNumber);
+  }
+
+  /// Returns current OnboardingInfo and increments the ScreenNumber
+  /// so that the next OnboardingInfo can be retrieved
+  Future<Either<Failure, OnboardInfo>> next() async {
+    final result = await repository.getOnboardingInfo(screenNum);
+    if (screenNum.index < ScreenNumber.values.length - 1) {
+      screenNum = ScreenNumber.values[screenNum.index + 1];
+    } else {
+      screenNum = ScreenNumber.zero;
+    }
+    return result;
   }
 }

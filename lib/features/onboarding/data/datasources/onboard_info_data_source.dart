7fda82fd1657aa3aba9d95392d6292a4c98db798
Winston <winstonyeo99@yahoo.com>
2020-03-26 10:33:12 +0800

    ✅ Add file not found test
    Red
    
    Pass File retrieve test - Green

--- lib/features/onboarding/data/datasources/onboard_info_data_source.dart
+++ lib/features/onboarding/data/datasources/onboard_info_data_source.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth_demo_flutter/features/onboarding/data/models/onboard_info_model.dart';
 import 'package:firebase_auth_demo_flutter/features/onboarding/domain/repositories/onboarding_repository.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 abstract class OnboardInfoDataSource {
@@ -12,5 +13,29 @@ class OnboardInfoDataSourceImpl implements OnboardInfoDataSource {
   AssetBundle assetBundle;
 
   @override
-  Future<OnboardInfoModel> getOnbaordInfo(ScreenNumber number) {}
+  Future<OnboardInfoModel> getOnbaordInfo(ScreenNumber number) async {
+    String details;
+    AssetImage image;
+    switch (number) {
+      case ScreenNumber.zero:
+        details = await assetBundle.loadString('key');
+        image = AssetImage('');
+        break;
+      case ScreenNumber.one:
+        details = await assetBundle.loadString('key');
+        image = AssetImage('');
+        break;
+      case ScreenNumber.two:
+        details = await assetBundle.loadString('key');
+        image = AssetImage('');
+        break;
+      case ScreenNumber.three:
+        details = await assetBundle.loadString('key');
+        image = AssetImage('');
+        break;
+      default:
+        break;
+    }
+    return Future.value(OnboardInfoModel.fromStringAndImage(details, image));
+  }
 }
--- test/onboarding/onboard_info_data_source_test.dart
+++ test/onboarding/onboard_info_data_source_test.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth_demo_flutter/core/error/exception.dart';
 import 'package:firebase_auth_demo_flutter/features/onboarding/data/datasources/onboard_info_data_source.dart';
 import 'package:firebase_auth_demo_flutter/features/onboarding/data/models/onboard_info_model.dart';
 import 'package:firebase_auth_demo_flutter/features/onboarding/domain/repositories/onboarding_repository.dart';
@@ -22,11 +23,32 @@ void main() {
   test('Retrieves an OnboardInfo', () async {
     const ScreenNumber tScreenNum = ScreenNumber.zero;
     final OnboardInfoModel tOnboardInfoModel = OnboardInfoModel(
-        title: 'test', description: 'body\nof description', image: null);
+        title: 'test',
+        description: 'body\nof description',
+        image: AssetImage(''));
 
     // Arrange
     when(assetBundle.loadString(any)).thenAnswer(
         (realInvocation) => Future.value(fixture('onboardInfoSample.txt')));
+
+    // Act
+    final result = await data.getOnbaordInfo(tScreenNum);
+
+    // Assert
+    expect(result, equals(tOnboardInfoModel));
+    verify(assetBundle.loadString(any));
+    verifyNoMoreInteractions(assetBundle);
+  });
+
+  test('Not able to retrieve OnboardInfo', () async {
+    const ScreenNumber tScreenNum = ScreenNumber.zero;
+    final OnboardInfoModel tOnboardInfoModel = OnboardInfoModel(
+        title: 'test',
+        description: 'body\nof description',
+        image: AssetImage(''));
+
+    // Arrange
+    when(assetBundle.loadString(any)).thenThrow(FileException());
 
     // Act
     final result = await data.getOnbaordInfo(tScreenNum);

09bc1beb24ba646759590ad263c43d57ab47c903
Winston <winstonyeo99@yahoo.com>
2020-04-01 19:16:16 +0800

    ➕ Dependency Injection for tuteeAssignment Feature

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cotor/core/platform/network_info.dart';
 import 'package:cotor/features/onboarding/data/datasources/onboard_info_data_source.dart';
 import 'package:cotor/features/onboarding/data/repositories/onboarding_repository_adapter.dart';
 import 'package:cotor/features/onboarding/domain/usecases/get_onboarding_info.dart';
 import 'package:cotor/features/onboarding/presentation/bloc/bloc.dart';
+import 'package:cotor/features/tutee_assignments/data/datasources/tutee_assignment_local_data_source.dart';
+import 'package:cotor/features/tutee_assignments/data/datasources/tutee_assignment_remote_data_source.dart';
+import 'package:cotor/features/tutee_assignments/data/repositories/tutee_assignment_repository_impl.dart';
+import 'package:cotor/features/tutee_assignments/domain/repositories/tutee_assignment_repo.dart';
+import 'package:cotor/features/tutee_assignments/domain/usecases/get_cached_tutee_assignment_list.dart';
+import 'package:cotor/features/tutee_assignments/domain/usecases/get_next_tutee_assignment_list.dart';
+import 'package:cotor/features/tutee_assignments/domain/usecases/get_tutee_assignment_list.dart';
+import 'package:cotor/features/tutee_assignments/presentation/bloc/bloc.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter/services.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'features/onboarding/domain/repositories/onboarding_repository.dart';
 
@@ -18,19 +30,45 @@ Future<void> init() async {
     ),
   );
 
+  sl.registerFactory<AssignmentsBloc>(
+    () => AssignmentsBloc(
+      getAssignmentList: sl(),
+      getNextAssignments: sl(),
+      getCachedAssignments: sl(),
+    ),
+  );
+
   // UseCase
   sl.registerLazySingleton(() => GetOnboardingInfo(repository: sl()));
+
+  sl.registerLazySingleton(() => GetTuteeAssignmentList(repo: sl()));
+  sl.registerLazySingleton(() => GetNextTuteeAssignmentList(repo: sl()));
+  sl.registerLazySingleton(() => GetCachedTuteeAssignmentList(repo: sl()));
 
   // Repository
   sl.registerLazySingleton<OnboardingRepository>(
       () => OnboardingRepositoryAdapter(dataSource: sl()));
+  sl.registerLazySingleton<TuteeAssignmentRepo>(() => TuteeAssignmentRepoImpl(
+        localDs: sl(),
+        remoteDs: sl(),
+        networkInfo: sl(),
+      ));
 
   // DataSources
   sl.registerLazySingleton<OnboardInfoDataSource>(
       () => OnboardInfoDataSourceImpl(sl()));
+  sl.registerLazySingleton<TuteeAssignmentRemoteDataSource>(
+      () => TuteeAssignmentRemoteDataSourceImpl(remoteStore: sl()));
+  sl.registerLazySingleton<TuteeAssignmentLocalDataSource>(
+      () => TuteeAssignmentLocalDataSourceImpl(sharedPreferences: sl()));
 
   //! Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 
   //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton<Firestore>(() => Firestore.instance);
+  sl.registerLazySingleton(() => DataConnectionChecker());
   sl.registerLazySingleton<AssetBundle>(() => rootBundle);
 }

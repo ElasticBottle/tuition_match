32878c1ad238d5e02113c58dd759539f4038bfc2
Winston <winstonyeo99@yahoo.com>
2020-04-06 21:29:49 +0800

    ✨Add extra functionality for core entitiy TuteeAssignment class
    Override toString method so that TuteeAssignment can be printed to console (might be a little late LoL)
    
    Add copyWith method so new instance can be created without having to manually specifiying every parameter

--- lib/domain/entities/tutee_assignment.dart
+++ lib/domain/entities/tutee_assignment.dart
@@ -46,6 +46,50 @@ class TuteeAssignment extends Equatable {
   final String photoUrl;
   final String dateAdded;
 
+  TuteeAssignment copyWith({
+    String postId,
+    Gender gender,
+    Level level,
+    Subject subject,
+    ClassFormat format,
+    String timing,
+    double rateMin,
+    double rateMax,
+    String location,
+    String freq,
+    TutorOccupation tutorOccupation,
+    String additionalRemarks,
+    Status status,
+    String username,
+    Name tuteeName,
+    int applied,
+    int liked,
+    String photoUrl,
+    String dateAdded,
+  }) {
+    return TuteeAssignment(
+      postId: postId ?? this.postId,
+      username: username ?? this.username,
+      tuteeName: tuteeName ?? this.tuteeName,
+      gender: gender ?? this.gender,
+      level: level ?? this.level,
+      subject: subject ?? this.subject,
+      format: format ?? this.format,
+      tutorOccupation: tutorOccupation ?? this.tutorOccupation,
+      timing: timing ?? this.timing,
+      freq: freq ?? this.freq,
+      location: location ?? this.location,
+      rateMax: rateMax ?? this.rateMax,
+      rateMin: rateMin ?? this.rateMin,
+      additionalRemarks: additionalRemarks ?? this.additionalRemarks,
+      status: status ?? this.status,
+      applied: applied ?? this.applied,
+      liked: liked ?? this.liked,
+      photoUrl: photoUrl ?? this.photoUrl,
+      dateAdded: dateAdded ?? this.dateAdded,
+    );
+  }
+
   @override
   List<Object> get props => [
         postId,
@@ -65,5 +109,30 @@ class TuteeAssignment extends Equatable {
         username,
         tuteeName,
         liked,
+        photoUrl,
+        dateAdded,
       ];
+
+  @override
+  String toString() => '''TuteeAssignmet {
+    postId : $postId ,
+    gender : $gender ,
+    level : $level ,
+    subject : $subject ,
+    format : $format ,
+    timing : $timing ,
+    rateMax : $rateMax ,
+    rateMax : $rateMax ,
+    location : $location ,
+    freq : $freq ,
+    tutorOccupation : $tutorOccupation ,
+    additionalRemarks : $additionalRemarks ,
+    status : $status ,
+    applied : $applied ,
+    username : $username ,
+    tuteeName : $tuteeName ,
+    liked : $liked ,
+    photoUrl : $photoUrl ,
+    dateAdded : $dateAdded ,
+    ''';
 }

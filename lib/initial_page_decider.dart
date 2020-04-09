b2005738e6cdf4d883d7a084123b1629a319b162
Winston <winstonyeo99@yahoo.com>
2020-04-09 17:39:13 +0800

    🎨 Swap out provider login manager for bloc pattern

--- lib/initial_page_decider.dart
+++ lib/initial_page_decider.dart
@@ -1,27 +1,35 @@
+import 'package:cotor/features/auth_service/bloc/auth_service_bloc/auth_service_bloc.dart';
 import 'package:cotor/features/onboarding/presentation/pages/onboard_page.dart';
-import 'package:cotor/features/sign-in/app/sign_in/sign_in_page.dart';
-import 'package:cotor/features/sign-in/services/auth_service.dart';
 import 'package:cotor/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// Builds the signed-in or non signed-in UI, depending on the user snapshot.
 /// This widget should be below the [MaterialApp].
 /// An [AuthWidgetBuilder] ancestor is required for this widget to work.
 /// Note: this class used to be called [LandingPage].
 class InitialPageDecider extends StatelessWidget {
-  const InitialPageDecider({Key key, @required this.userSnapshot})
-      : super(key: key);
-  final AsyncSnapshot<User> userSnapshot;
-
   @override
   Widget build(BuildContext context) {
-    if (userSnapshot.connectionState == ConnectionState.active) {
-      return userSnapshot.hasData ? HomePage() : OnboardPage();
-    }
-    return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
+    return BlocBuilder<AuthServiceBloc, AuthServiceState>(
+      builder: (context, state) {
+        if (state is Uninitialized) {
+          return OnboardPage();
+        }
+        if (state is Authenticated) {
+          if (state.isEmailVerified) {
+            return HomePage();
+          } else {
+            return Container(color: Colors.red);
+          }
+        }
+        if (state is Unauthenticated) {}
+        return Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,37 @@
 import 'package:cotor/core/bloc/bloc_delegate.dart';
-import 'package:cotor/features/sign-in/app/email_link_error_presenter.dart';
-import 'package:cotor/features/sign-in/services/auth_service.dart';
-import 'package:cotor/features/sign-in/services/auth_service_adapter.dart';
-import 'package:cotor/features/sign-in/services/email_secure_store.dart';
-import 'package:cotor/features/sign-in/services/firebase_email_link_handler.dart';
+import 'package:cotor/features/auth_service/bloc/auth_service_bloc/auth_service_bloc.dart';
 import 'package:cotor/initial_page_decider.dart';
-import 'package:cotor/user_data_injector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:provider/provider.dart';
 import 'package:cotor/injection_container.dart' as di;
 import 'package:cotor/routing/router.gr.dart';
+
+import 'injection_container.dart';
 
 Future<void> main() async {
   // Fix for: Unhandled Exception: ServicesBinding.defaultBinaryMessenger was accessed before the binding was initialized.
   WidgetsFlutterBinding.ensureInitialized();
   BlocSupervisor.delegate = SimpleBlocDelegate();
   await di.init();
-  runApp(MyApp());
+  runApp(
+    BlocProvider(
+      create: (context) => sl<AuthServiceBloc>()..add(AppStarted()),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   // [initialAuthServiceType] is made configurable for testing
-  const MyApp({
-    this.initialAuthServiceType = AuthServiceType.firebase,
-  });
-  final AuthServiceType initialAuthServiceType;
+  const MyApp();
 
   @override
   Widget build(BuildContext context) {
-    // MultiProvider for top-level services that can be created right away
-    return MultiProvider(
-      providers: [
-        Provider<AuthService>(
-          create: (_) => AuthServiceAdapter(
-            initialAuthServiceType: initialAuthServiceType,
-          ),
-          dispose: (_, AuthService authService) => authService.dispose(),
-        ),
-        Provider<EmailSecureStore>(
-          create: (_) => EmailSecureStore(
-            flutterSecureStorage: FlutterSecureStorage(),
-          ),
-        ),
-        ProxyProvider2<AuthService, EmailSecureStore, FirebaseEmailLinkHandler>(
-          update: (_, AuthService authService, EmailSecureStore storage, __) =>
-              FirebaseEmailLinkHandler.createAndConfigure(
-            auth: authService,
-            userCredentialsStorage: storage,
-          ),
-          dispose: (_, linkHandler) => linkHandler.dispose(),
-        ),
-      ],
-      child: UserDataInjector(
-          builder: (BuildContext context, AsyncSnapshot<User> userSnapshot) {
-        return MaterialApp(
-          theme: ThemeData(primarySwatch: Colors.indigo),
-          debugShowCheckedModeBanner: false,
-          home: EmailLinkErrorPresenter(
-            context: context,
-            child: InitialPageDecider(userSnapshot: userSnapshot),
-          ),
-          onGenerateRoute: Router().onGenerateRoute,
-        );
-      }),
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      debugShowCheckedModeBanner: false,
+      home: InitialPageDecider(),
+      onGenerateRoute: Router().onGenerateRoute,
     );
 
     // return MaterialApp(

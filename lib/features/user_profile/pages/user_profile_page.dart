b146a19aed8acb3eaed35c884be3946904f671a7
Winston <winstonyeo99@yahoo.com>
2020-04-23 14:47:52 +0800

    ♻️ Refactor User Profile page to utilize CacheBadge

--- lib/features/user_profile/pages/user_profile_page.dart
+++ lib/features/user_profile/pages/user_profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:cotor/common_widgets/bars/custom_sliver_app_bar.dart';
 import 'package:cotor/common_widgets/buttons/custom_raised_button.dart';
 import 'package:cotor/common_widgets/information_display/avatar.dart';
+import 'package:cotor/common_widgets/information_display/custom_snack_bar.dart';
 import 'package:cotor/constants/custom_color_and_fonts.dart';
 import 'package:cotor/constants/strings.dart';
 import 'package:cotor/features/add_tutee_assignment/bloc/edit_tutee_assignment_bloc.dart';
@@ -9,7 +10,7 @@ import 'package:cotor/features/models/tutee_assignment_model.dart';
 import 'package:cotor/features/models/tutor_profile_model.dart';
 import 'package:cotor/features/models/user_model.dart';
 import 'package:cotor/features/tutee_assignment_list/widgets/assignment_item_tile.dart';
-import 'package:cotor/features/user_profile/bloc/user_profile_page_bloc.dart';
+import 'package:cotor/features/user_profile/widgets/cache_badge.dart';
 import 'package:cotor/features/user_profile_bloc/user_profile_bloc.dart';
 import 'package:cotor/routing/router.gr.dart';
 import 'package:flutter/material.dart';
@@ -25,131 +26,110 @@ class UserProfilePage extends StatelessWidget {
         BlocProvider.of<UserProfileBloc>(context);
     final EditTuteeAssignmentBloc editTuteeAssignmentBloc =
         BlocProvider.of<EditTuteeAssignmentBloc>(context);
-    return BlocListener<UserProfileBloc, UserProfileState>(
+    return BlocConsumer<UserProfileBloc, UserProfileState>(
       bloc: userProfileBloc,
       listener: (context, state) {
+        print('listener called');
         if (state.updateProfileSuccess) {
           Scaffold.of(context)
             ..hideCurrentSnackBar()
             ..showSnackBar(
-              SnackBar(
-                content: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(Icons.check),
-                    SizedBox(width: 20.0),
-                    Expanded(child: Text(state.updateProfileSuccessMsg))
-                  ],
-                ),
-                duration: Duration(seconds: 2),
-                action: SnackBarAction(
-                  label: Strings.dismiss,
-                  onPressed: () {
-                    Scaffold.of(context).hideCurrentSnackBar();
-                  },
-                ),
-                backgroundColor: Colors.green,
-              ),
+              CustomSnackBar(
+                message: state.updateProfileSuccessMsg,
+                isError: false,
+              ).show(context),
             );
         }
       },
-      child: CustomScrollView(
-        slivers: [
-          CustomSliverAppbar(
-            title: Strings.yourProfile,
-          ),
-          BlocBuilder<UserProfilePageBloc, UserProfilePageState>(
-            bloc: BlocProvider.of<UserProfilePageBloc>(context),
-            builder: (context, state) {
-              return SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    Avatar(
-                      photoUrl:
-                          userProfileBloc.state.userProfile.photoUrl.isEmpty
-                              ? null
-                              : userProfileBloc.state.userProfile.photoUrl,
-                      radius: MediaQuery.of(context).size.width / 4,
+      builder: (context, state) {
+        final List<TuteeAssignmentModel> assignments =
+            state.userProfile.userAssignments.values.toList();
+        return CustomScrollView(
+          slivers: [
+            CustomSliverAppbar(
+              title: Strings.yourProfile,
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Avatar(
+                    photoUrl: state.userProfile.photoUrl.isEmpty
+                        ? null
+                        : state.userProfile.photoUrl,
+                    radius: MediaQuery.of(context).size.width / 4,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  if (state.userProfile.isTutor)
+                    TutorDetails(
+                      tutorProfileModel: state.userProfile.tutorProfile,
+                      isEditable: true,
+                      editTutorProfileBloc: editTutorProfileBloc,
+                      userDetails: state.userProfile,
+                      hasCacheProfile: state.hasCachedProfile,
                     ),
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    if (userProfileBloc.state.userProfile.isTutor)
-                      TutorDetails(
-                        tutorProfileModel:
-                            userProfileBloc.state.userProfile.tutorProfile,
-                        isEditable: true,
-                        editTutorProfileBloc: editTutorProfileBloc,
-                        userDetails: userProfileBloc.state.userProfile,
-                      ),
-                    if (!userProfileBloc.state.userProfile.isTutor)
-                      CustomRaisedButton(
+                  if (!state.userProfile.isTutor)
+                    CacheBadge(
+                      hasCacheProfile: state.hasCachedProfile,
+                      child: CustomRaisedButton(
                         width: MediaQuery.of(context).size.width / 1.8,
                         onPressed: () {
-                          editTutorProfileBloc
-                            ..add(
-                              InitialiseProfileFields(
+                          editTutorProfileBloc.add(
+                            InitialiseProfileFields(
                                 tutorProfile: TutorProfileModel(),
-                                userDetails: userProfileBloc.state.userProfile,
-                              ),
-                            );
-                          Navigator.of(context, rootNavigator: true)
-                              .pushNamed(Routes.editTutorPage);
+                                userDetails: state.userProfile,
+                                isCacheProfile: state.hasCachedProfile),
+                          );
+                          Navigator.of(context).pushNamed(Routes.editTutorPage);
                         },
                         textColor: ColorsAndFonts.backgroundColor,
                         color: ColorsAndFonts.primaryColor,
                         child: Text(Strings.startTeachingNow),
                       ),
-                    Divider(
-                      height: 50.0,
-                      thickness: 5.0,
                     ),
-                    Text(Strings.myAssignment)
-                  ],
-                ),
-              );
-            },
-          ),
-          BlocBuilder<UserProfilePageBloc, UserProfilePageState>(
-            builder: (context, state) {
-              final List<TuteeAssignmentModel> assignments = userProfileBloc
-                  .state.userProfile.userAssignments.values
-                  .toList();
-              return SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
-                    return AssignmentItemTile(
-                      assignment: assignments[index],
-                    );
-                  },
-                  childCount:
-                      userProfileBloc.state.userProfile.userAssignments.length,
-                ),
-              );
-            },
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                CustomRaisedButton(
-                  onPressed: () {
-                    editTuteeAssignmentBloc.add(
-                      InitialiseEditTuteeFields(
-                          assignment: TuteeAssignmentModel(),
-                          userDetails: userProfileBloc.state.userProfile),
-                    );
-                    Navigator.of(context).pushNamed(Routes.editAssignmentPage);
-                  },
-                  width: MediaQuery.of(context).size.width / 1.8,
-                  textColor: ColorsAndFonts.backgroundColor,
-                  color: ColorsAndFonts.primaryColor,
-                  child: Text(Strings.addAssignment),
-                ),
-              ],
+                  Divider(
+                    height: 50.0,
+                    thickness: 5.0,
+                  ),
+                  Text(Strings.myAssignment)
+                ],
+              ),
             ),
-          ),
-        ],
-      ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return AssignmentItemTile(
+                    assignment: assignments[index],
+                  );
+                },
+                childCount: state.userProfile.userAssignments.length,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  CustomRaisedButton(
+                    onPressed: () {
+                      editTuteeAssignmentBloc.add(
+                        InitialiseEditTuteeFields(
+                            assignment: TuteeAssignmentModel(),
+                            userDetails: state.userProfile),
+                      );
+                      Navigator.of(context)
+                          .pushNamed(Routes.editAssignmentPage);
+                    },
+                    width: MediaQuery.of(context).size.width / 1.8,
+                    textColor: ColorsAndFonts.backgroundColor,
+                    color: ColorsAndFonts.primaryColor,
+                    child: Text(Strings.addAssignment),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -157,12 +137,14 @@ class UserProfilePage extends StatelessWidget {
 class TutorDetails extends StatelessWidget {
   const TutorDetails({
     @required this.tutorProfileModel,
-    this.isEditable = false,
     this.editTutorProfileBloc,
     this.userDetails,
+    this.isEditable = false,
+    this.hasCacheProfile = false,
   });
   final TutorProfileModel tutorProfileModel;
   final bool isEditable;
+  final bool hasCacheProfile;
   final EditTutorProfileBloc editTutorProfileBloc;
   final UserModel userDetails;
 
@@ -175,22 +157,24 @@ class TutorDetails extends StatelessWidget {
           children: <Widget>[
             Text(Strings.bio),
             if (isEditable)
-              IconButton(
-                icon: FaIcon(
-                  FontAwesomeIcons.edit,
-                  color: ColorsAndFonts.userProfilePageEditProfileIconColour,
-                ),
-                onPressed: () {
-                  editTutorProfileBloc
-                    ..add(
+              CacheBadge(
+                hasCacheProfile: hasCacheProfile,
+                child: IconButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.edit,
+                    color: ColorsAndFonts.userProfilePageEditProfileIconColour,
+                  ),
+                  onPressed: () {
+                    editTutorProfileBloc.add(
                       InitialiseProfileFields(
                         tutorProfile: tutorProfileModel,
                         userDetails: userDetails,
+                        isCacheProfile: hasCacheProfile,
                       ),
                     );
-                  Navigator.of(context, rootNavigator: true)
-                      .pushNamed(Routes.editTutorPage);
-                },
+                    Navigator.of(context).pushNamed(Routes.editTutorPage);
+                  },
+                ),
               ),
           ],
         ),
---
+++ lib/features/user_profile/widgets/cache_badge.dart
@@ -0,0 +1,27 @@
+import 'package:badges/badges.dart';
+import 'package:flutter/material.dart';
+
+class CacheBadge extends StatelessWidget {
+  const CacheBadge({
+    Key key,
+    @required this.hasCacheProfile,
+    @required this.child,
+  }) : super(key: key);
+
+  final bool hasCacheProfile;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Badge(
+        showBadge: hasCacheProfile,
+        badgeColor: Colors.orange,
+        badgeContent: Text('x'),
+        position: BadgePosition(
+          right: 5.0,
+          top: 2.0,
+        ),
+        shape: BadgeShape.circle,
+        child: child);
+  }
+}

91d3a678d06838f95d1634c96cd87a0da09e536c
Winston <winstonyeo99@yahoo.com>
2020-05-03 02:06:30 +0800

    ✏️  Fix class name
    Change to ForgotPasswordPage from RegistrationPage

--- lib/features/auth_service/forgot_password/pages/forgot_password_page.dart
+++ lib/features/auth_service/forgot_password/pages/forgot_password_page.dart
@@ -1,11 +1,11 @@
+import 'package:cotor/features/auth_service/forgot_password/pages/forgot_password_form.dart';
 import 'package:cotor/features/auth_service/registration/bloc/registration_bloc.dart';
-import 'package:cotor/features/auth_service/registration/pages/registration_form.dart';
 import 'package:cotor/injection_container.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class RegistrationPage extends StatelessWidget {
-  const RegistrationPage({Key key}) : super(key: key);
+class ForgotPasswordPage extends StatelessWidget {
+  const ForgotPasswordPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,7 @@ class RegistrationPage extends StatelessWidget {
       body: Center(
         child: BlocProvider<RegistrationBloc>(
           create: (context) => sl<RegistrationBloc>(),
-          child: RegistrationForm(),
+          child: ForgotPasswordForm(),
         ),
       ),
     );
